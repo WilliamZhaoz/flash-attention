e6136e4eaab70d7fe86e77f96207e6f9a2d75fce
zhiyzh <zhiyzh@microsoft.com>
2023-07-13 18:09:04 +0000

    test time

--- benchmarks/benchmark_flash_attention_ca.py
+++ benchmarks/benchmark_flash_attention_ca.py
@@ -1,5 +1,6 @@
 import math
 import torch
+import time
 import torch.nn.functional as F
 
 from einops import rearrange, repeat
@@ -19,7 +20,7 @@ def attention_ref(q, k, v, attn_mask, dropout_p, upcast=False, causal=False):
         output: (batch_size, seqlen, nheads, head_dim)
         attention: softmax after dropout
     """
-
+    '''
     # seqlen_q = q.shape[1]
     # seqlen_k = k.shape[1]
     # seqlen_v = v.shape[1]
@@ -30,7 +31,24 @@ def attention_ref(q, k, v, attn_mask, dropout_p, upcast=False, causal=False):
     attention_drop = F.dropout(attention, dropout_p)
     output = torch.einsum('bhts,bshd->bthd', attention_drop, v)
     # return output.to(dtype=qkv.dtype), attention.to(dtype=qkv.dtype)
-    return output.to(dtype=q.dtype)
+    '''
+    h = q.shape[2]
+    dim_head = q.shape[3]
+
+    q, k, v = map(lambda t: rearrange(t, "b n h d -> (b h) n d", h=h), (q, k, v))
+
+    scale = dim_head**-0.5
+    # force cast to fp32 to avoid overflowing
+    with torch.autocast(enabled=False, device_type="cuda"):
+        q, k, v = q.float(), k.float(), v.float()
+        sim = torch.einsum("b i d, b j d -> b i j", q, k) * scale
+
+    # attention, what we cannot get enough of
+    sim = sim.softmax(dim=-1)
+
+    out = torch.einsum("b i j, b j d -> b i d", sim, v)
+    out = rearrange(out, "(b h) n d -> b n (h d)", h=h)
+    return out.to(dtype=q.dtype)
 
 
 torch.manual_seed(0)
@@ -38,7 +56,7 @@ repeats = 30
 batch_size = 64
 nheads = 16
 
-seqlen_q = 1024
+seqlen_q = 2048
 seqlen_k = 1024
 seqlen_v = 1024
 
@@ -84,15 +102,45 @@ q = rearrange(Wqkv(q), 'b s (h d) -> b s h d', h=nheads).detach().requires_grad_
 k = rearrange(Wqkv(k), 'b s (h d) -> b s h d', h=nheads).detach().requires_grad_()
 v = rearrange(Wqkv(v), 'b s (h d) -> b s h d', h=nheads).detach().requires_grad_()
 
-fn = lambda q_unpad, k_unpad, v_unpad: flash_attn_unpadded_func(q_unpad,
-                                                                k_unpad,
-                                                                v_unpad,
-                                                                cu_seqlens_q,
-                                                                cu_seqlens_k,
-                                                                max_seqlen_in_batch_q,
-                                                                max_seqlen_in_batch_k,
-                                                                dropout_p,
-                                                                causal=causal)
-benchmark_all(fn, q_unpad, k_unpad, v_unpad, repeats=repeats, desc='FlashAttention')
-fn = lambda q, k, v: attention_ref(q, k, v, attention_mask_bool_q, dropout_p, causal=causal)
-benchmark_all(fn, q, k, v, repeats=repeats, desc='PyTorch Standard Attention')
+v_unpad = k_unpad
+
+fn1 = lambda q_unpad, k_unpad, v_unpad: flash_attn_unpadded_func(q_unpad,
+                                                                 k_unpad,
+                                                                 v_unpad,
+                                                                 cu_seqlens_q,
+                                                                 cu_seqlens_k,
+                                                                 max_seqlen_in_batch_q,
+                                                                 max_seqlen_in_batch_k,
+                                                                 dropout_p,
+                                                                 causal=causal)
+# q_unpad, k_unpad, v_unpad = q_unpad.to(torch.float16), k_unpad.to(torch.float16), v_unpad.to(torch.float16)
+start = time.time()
+benchmark_all(fn1, q_unpad, k_unpad, v_unpad, repeats=repeats, desc='FlashAttention')
+# print("==============", time.time() - start)
+
+fn2 = lambda q, k, v: attention_ref(q, k, v, attention_mask_bool_q, dropout_p, causal=causal)
+start = time.time()
+benchmark_all(fn2, q, k, v, repeats=repeats, desc='PyTorch Standard Attention')
+# print("==============", time.time() - start)
+
+# test by myself
+
+start = time.time()
+for i in range(10):
+    with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=False):
+        out = flash_attn_unpadded_func(q_unpad,
+                                       k_unpad,
+                                       v_unpad,
+                                       cu_seqlens_q,
+                                       cu_seqlens_k,
+                                       max_seqlen_in_batch_q,
+                                       max_seqlen_in_batch_k,
+                                       dropout_p,
+                                       causal=causal)
+print(time.time() - start)
+
+start = time.time()
+for i in range(10):
+    with torch.autocast(device_type='cuda', dtype=torch.float16, enabled=False):
+        out = attention_ref(q, k, v, attention_mask_bool_q, dropout_p, causal=causal)
+print(time.time() - start)
